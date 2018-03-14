187e479feacb2b87c830e308d4c8e60b7f767aab
Kadir Özdemir <ko@webberz.at>
2018-03-14 08:54:28 +0100

    my overrides

---
+++ .my_custom_zsh_path/my_overrides.zsh
@@ -0,0 +1,22 @@
+# Checks if working tree is dirty
+function parse_git_dirty() {
+  local STATUS=''
+  local -a FLAGS
+  FLAGS=('--porcelain')
+  if [[ "$(command git config --get oh-my-zsh.hide-dirty)" != "1" ]]; then
+    if [[ $POST_1_7_2_GIT -gt 0 ]]; then
+      FLAGS+='--ignore-submodules=dirty'
+    fi
+    if [[ "$DISABLE_UNTRACKED_FILES_DIRTY" == "true" ]]; then
+      FLAGS+='--untracked-files=no'
+    fi
+    STATUS=$(command git status ${FLAGS} 2> /dev/null)
+  fi
+  if $(echo "$STATUS" | command grep -E '^\?\? ' &> /dev/null); then
+      echo "$ZSH_THEME_GIT_PROMPT_UNTRACKED"
+  elif [[ -n $STATUS ]]; then
+    echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
+  else
+    echo "$ZSH_THEME_GIT_PROMPT_CLEAN"
+  fi
+}
--- .my_custom_zsh_path/themes/prose.zsh-theme
+++ .my_custom_zsh_path/themes/prose.zsh-theme
@@ -22,7 +22,7 @@ $ '
 ZSH_THEME_GIT_PROMPT_PREFIX=" on %{$fg[magenta]%}\uE0A0 "
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
 
-ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[green]%}?"
+ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[green]%} ✱ %{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_DIRTY="%{$reset_color%}%{$fg[red]%} ✘ %{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[green]%} ✔ %{$reset_color%}"
 
